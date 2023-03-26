86f1409c8745fe0849e037efba8ff3577c104904
tsumitakaoru <k.tsumita01@gmail.com>
2023-03-26 10:55:43 -0400

    Add role section

--- src/client/pages/Profile.js
+++ src/client/pages/Profile.js
@@ -26,6 +26,8 @@ const Profile = () => {
   // Check if current user is an admin
   const [adminMode, setAdminMode] = useState(false);
 
+  const [isAdminProfile, setIsAdminProfile] = useState(false);
+
   useEffect(() => {
     if (user) {
       db.collection(USERS)
@@ -34,6 +36,17 @@ const Profile = () => {
         .then((currentUser) => setAdminMode(currentUser.data().isAdmin));
     }
   }, []);
+
+  useEffect(() => {
+    if (userDoc) {
+      setIsAdminProfile(userDoc.isAdmin);
+    }
+  }, [userDoc]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    return updateUser(userDoc.uid, { isAdmin: !isAdminProfile });
+  };
 
   return (
     <main>
@@ -56,6 +69,27 @@ const Profile = () => {
           </>
         )}
       </LoadingError>
+
+      <Card>
+        <div className="sm:col-span-6">
+          <label htmlFor="isAdmin" className="block text-sm font-medium text-gray-700">
+            Role
+          </label>
+          <p className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-md sm:text-sm border-gray-300">{userDoc && isAdminProfile ? 'Admin' : 'User'}</p>
+          {userDoc && adminMode && !isAdminProfile && (
+            <div className="pt- 5 flex justify-end">
+              <button
+                type="submit"
+                className="ml-3 inline-flex   justify-center py-2 px-4 border   border-transparent shadow-sm  text-sm font-medium rounded-md   text-white bg-indigo-600  hover:bg-indigo-700  focus:outline-none focus:ring-2  focus:ring-offset-2  focus:ring-indigo-500"
+                onClick={handleSubmit}
+              >
+                Grant Admin Access
+              </button>
+            </div>
+          )
+          }
+        </div>
+      </Card>
     </main>
   );
 };
